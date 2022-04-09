cce841b46c89e99868bc732cea87b371e6be7092
sorxcode <sorxcode@gmail.com>
2022-04-09 11:17:12 +0100

    checkpoint

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
+
+import requests
 from dotenv import load_dotenv
 from flask import Flask
-import requests
 
 load_dotenv()
-CONVALENT_API_KEY = os.getenv('COVALENT_API_KEY')
+COVALENT_API_KEY = os.getenv("COVALENT_API_KEY")
+CHAIN_ID = "1"  # ethereun chain
 app = Flask(__name__)
 
 
@@ -17,11 +19,31 @@ def home():
 def get_erc20_transactions(address, contract_address):
     """
     args:
-    address: EOA
+    address: sender address
     contract_address: token address
     """
-    CHAIN_ID = '1'  # ethereun chain
-    url = 'https://api.covalenthq.com/v1/{CHAIN_ID}/address/{EOA}/transfers_v2/?quote-currency=USD&format=JSON&contract-address={CONTRACT_ADDRESS}&key={COVALENT_API_KEY}'
-    response = requests.get(url.format(CHAIN_ID=CHAIN_ID, EOA=address, CONTRACT_ADDRESS=contract_address, COVALENT_API_KEY=CONVALENT_API_KEY))
+    url = "https://api.covalenthq.com/v1/{CHAIN_ID}/address/{ADDRESS}/transfers_v2/?quote-currency=USD&format=JSON&contract-address={CONTRACT_ADDRESS}&key={COVALENT_API_KEY}"
+    response = requests.get(
+        url.format(
+            CHAIN_ID=CHAIN_ID,
+            ADDRESS=address,
+            CONTRACT_ADDRESS=contract_address,
+            COVALENT_API_KEY=COVALENT_API_KEY,
+        )
+    )
+    result = response.json()
+    return result, response.status_code
+
+
+@app.route("/transactions/<string:address>")
+def get_transactions(address):
+    url = "https://api.covalenthq.com/v1/{CHAIN_ID}/address/{ADDRESS}/transactions_v2/?quote-currency=USD&format=JSON&block-signed-at-asc=false&no-logs=false&key={COVALENT_API_KEY}"
+    response = requests.get(
+        url.format(
+            CHAIN_ID=CHAIN_ID,
+            ADDRESS=address,
+            COVALENT_API_KEY=COVALENT_API_KEY
+        )
+    )
     result = response.json()
     return result, response.status_code
