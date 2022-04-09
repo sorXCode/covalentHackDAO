44c8bf836c6bd926a5ce5caf3410cc9130d47a8c
sorxcode <sorxcode@gmail.com>
2022-04-09 11:55:09 +0100

    added all endpoints

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import os
-
-import requests
 from dotenv import load_dotenv
 from flask import Flask
+from covalent_api.class_a import ClassA
+from covalent_api.session import Session
 
 load_dotenv()
-COVALENT_API_KEY = os.getenv("COVALENT_API_KEY")
+
+session = Session(server_url="https://api.covalenthq.com", api_key=os.getenv("API_KEY"))
+covalent = ClassA(session)
 CHAIN_ID = "1"  # ethereun chain
+
 app = Flask(__name__)
 
 
@@ -15,35 +18,30 @@ def home():
     return {"ho": "me"}
 
 
-@app.route("/transactions_erc20/<string:address>/<string:contract_address>")
+@app.route("/transactions/erc20/<string:address>/<string:contract_address>")
 def get_erc20_transactions(address, contract_address):
     """
     args:
     address: sender address
     contract_address: token address
     """
-    url = "https://api.covalenthq.com/v1/{CHAIN_ID}/address/{ADDRESS}/transfers_v2/?quote-currency=USD&format=JSON&contract-address={CONTRACT_ADDRESS}&key={COVALENT_API_KEY}"
-    response = requests.get(
-        url.format(
-            CHAIN_ID=CHAIN_ID,
-            ADDRESS=address,
-            CONTRACT_ADDRESS=contract_address,
-            COVALENT_API_KEY=COVALENT_API_KEY,
-        )
-    )
-    result = response.json()
-    return result, response.status_code
+    result = covalent.get_erc20_token_transfers(CHAIN_ID, address, contract_address)
+    return result
 
 
 @app.route("/transactions/<string:address>")
 def get_transactions(address):
-    url = "https://api.covalenthq.com/v1/{CHAIN_ID}/address/{ADDRESS}/transactions_v2/?quote-currency=USD&format=JSON&block-signed-at-asc=false&no-logs=false&key={COVALENT_API_KEY}"
-    response = requests.get(
-        url.format(
-            CHAIN_ID=CHAIN_ID,
-            ADDRESS=address,
-            COVALENT_API_KEY=COVALENT_API_KEY
-        )
-    )
-    result = response.json()
-    return result, response.status_code
+    result = covalent.get_transactions(CHAIN_ID, address)
+    return result
+
+
+@app.route("/balances/<string:address>")
+def get_token_balances(address):
+    result = covalent.get_token_balances_for_address(CHAIN_ID, address)
+    return result
+
+
+@app.route("/portfolio/<string:address>")
+def get_historical_portfolio(address):
+    result = covalent.get_historical_portfolio_value_over_time(CHAIN_ID, address)
+    return result
